54a85a948ed1fff79e8a14c888aa2a60d258e9ff
Leksa <lexysnake@gmail.com>
2023-04-11 20:39:47 +0300

    Использование компонента с массивом данных

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 
+const promoData = ["=)", "^_^", "O_o", "x_x", "=(", ";(", "0l0"];
+// .map() => преобразовывает один елемент массива в другой элемент (для всех без исключения)
+
+
 const Promo = (props) => {
     // props - объект - используется для передачи разных значений внутрь компонента
     return <div className="promo__block">
@@ -8,14 +12,30 @@ const Promo = (props) => {
 }
 
 const App = () => {
+    const smiles = []
+    for (let i = 0; i < promoData.length; i++) {
+        if (i % 2) { // остаток от деления = true => !0
+            smiles.push(<Promo text={promoData[i]}/>)
+        }
+    }
     return (
       <div>
         <h1>First Page</h1>
         <div className="container">
-            <Promo text="First"/>
+            {/* <Promo text="First"/>
             <Promo text="Second"/>
             <Promo text={4 * 10}/>
+            <Promo/> */}
+            {
+                // [<span>1</span>,2,<div>3</div>,4,<a href="">5</a>]
+            }
+            {promoData.map(el => <Promo text={el}/>)}
             <Promo/>
+            {promoData.map(el => <Promo text={el}/>)}
+
+            <Promo text={promoData[1]}/>
+            <Promo text={promoData[2]}/>
+            {smiles}
         </div>
       </div>
     )
